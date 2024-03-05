debe5d5aef26f97741802c6688bfca49c7eeb9e9
Vinicius Martins <vinicius.martins1196@gmail.com>
2024-03-05 00:10:57 -0300

    feat: ajuste no rds e no github actions

--- rds.tf
+++ rds.tf
@@ -1,128 +1,37 @@
-data "aws_caller_identity" "current" {}
-data "aws_availability_zones" "available" {}
+resource "aws_db_subnet_group" "subnet_group" {
+  name       = "subnet_group_rds"
+  subnet_ids = ["subnet-063e56bb28d5750f7", "subnet-0e7cf64a4ce1aeb12"]  # Substitua pelos IDs das subnets onde o RDS deve ser criado
+}
 
-locals {
-  name    = "complete-postgresql"
-  region  = "us-east-1"
+resource "aws_secretsmanager_secret" "ze_burguer_db_password" {
+  name = "password_ze_burguer_db"
+}
 
-  vpc_cidr = "10.0.0.0/16"
-  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+resource "aws_secretsmanager_secret_version" "example_db_password_version" {
+  secret_id     = aws_secretsmanager_secret.ze_burguer_db_password.id
+  secret_string = random_password.password.result
+}
+
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "_%@"
+}
+
+resource "aws_db_instance" "zeburguer_db" {
+  identifier            = "zeburger-db"
+  allocated_storage     = 20
+  storage_type          = "gp2"
+  engine                = "postgres"
+  engine_version        = "14"
+  instance_class        = "db.t4g.micro"
+  db_name               = "zeburguer"
+  username              = "zeburguer"
+  password              = random_password.password.result
+  db_subnet_group_name  = aws_db_subnet_group.subnet_group.name
+  final_snapshot_identifier = "false"
 
   tags = {
-    Name       = local.name
-    Example    = local.name
-    Repository = "https://github.com/terraform-aws-modules/terraform-aws-rds"
+    Name = "zeburger-db"
   }
-}
-
-################################################################################
-# RDS Module
-################################################################################
-
-module "db" {
-  source = "terraform-aws-modules/rds/aws"
-
-  identifier = local.name
-
-  # All available versions: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_PostgreSQL.html#PostgreSQL.Concepts
-  engine               = "postgres"
-  engine_version       = "14"
-  family               = "postgres14" # DB parameter group
-  major_engine_version = "14"         # DB option group
-  instance_class       = "db.t4g.micro"
-
-  allocated_storage     = 20
-  max_allocated_storage = 100
-
-  # NOTE: Do NOT use 'user' as the value for 'username' as it throws:
-  # "Error creating DB Instance: InvalidParameterValue: MasterUsername
-  # user cannot be used as it is a reserved word used by the engine"
-  db_name  = "zeburguerdb"
-  username = "zeburguerdb"
-  port     = 5432
-
-  # setting manage_master_user_password_rotation to false after it
-  # has been set to true previously disables automatic rotation
-  manage_master_user_password_rotation              = true
-  master_user_password_rotate_immediately           = false
-  master_user_password_rotation_schedule_expression = "rate(15 days)"
-
-  multi_az               = false
-  db_subnet_group_name   = module.vpc.database_subnet_group
-  vpc_security_group_ids = [module.security_group.security_group_id]
-
-  maintenance_window              = "Mon:00:00-Mon:03:00"
-  backup_window                   = "03:00-06:00"
-  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
-  create_cloudwatch_log_group     = true
-
-  backup_retention_period = 1
-  skip_final_snapshot     = true
-  deletion_protection     = false
-
-  performance_insights_enabled          = true
-  performance_insights_retention_period = 7
-  create_monitoring_role                = true
-  monitoring_interval                   = 60
-  monitoring_role_name                  = "example-monitoring-role-name"
-  monitoring_role_use_name_prefix       = true
-  monitoring_role_description           = "Description for monitoring role"
-
-  parameters = [
-    {
-      name  = "autovacuum"
-      value = 1
-    },
-    {
-      name  = "client_encoding"
-      value = "utf8"
-    }
-  ]
-
-  tags = local.tags
-  db_option_group_tags = {
-    "Sensitive" = "low"
-  }
-  db_parameter_group_tags = {
-    "Sensitive" = "low"
-  }
-}
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0"
-
-  name = local.name
-  cidr = local.vpc_cidr
-
-  azs              = local.azs
-  public_subnets   = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
-  private_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 3)]
-  database_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 6)]
-
-  create_database_subnet_group = true
-
-  tags = local.tags
-}
-
-module "security_group" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "~> 5.0"
-
-  name        = local.name
-  description = "Complete PostgreSQL example security group"
-  vpc_id      = module.vpc.vpc_id
-
-  # ingress
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 5432
-      to_port     = 5432
-      protocol    = "tcp"
-      description = "PostgreSQL access from within VPC"
-      cidr_blocks = module.vpc.vpc_cidr_block
-    },
-  ]
-
-  tags = local.tags
 }
